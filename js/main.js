5e3908c1d56cea40bf506529db88d7c24e0cf0c2
Duxcoder <iinegve_25@mail.ru>
2022-12-07 19:10:16 +0300

    feat: complite style for hamburger

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function hamburgerMenu () {
+    const icon = document.querySelector('.hamburger');
+    const navigation = document.querySelector('nav');
+    icon.addEventListener('click', () => {
+        navigation.classList.toggle('view-mobile');
+    })
+    window.addEventListener('resize', () => {
+        document.documentElement.clientWidth >= 768 && navigation.classList.remove('view-mobile');
+    });
+}
+
+hamburgerMenu()
